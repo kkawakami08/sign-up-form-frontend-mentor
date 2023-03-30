8f5c4db567e53218c85b89553e0c1ae8f1138516
Kaori Kawakami <k.kawakami08@gmail.com>
2023-03-30 15:36:05 -0700

    Initial commit and template for project with page style instantiated in tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primaryRed: "hsl(var(--primary-red))",
+        primaryGreen: "hsl(var(--primary-green))",
+        accentBlue: "hsl(var(--accent-blue))",
+        neutralDarkBlue: "hsl(var(--neutral-dark-blue))",
+        neutralGrayishBlue: "hsl(var(--neutral-grayish-blue))",
+      },
+      fontFamily: {
+        sans: ["Poppins", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
